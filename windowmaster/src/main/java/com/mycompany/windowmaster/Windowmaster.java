cc52206e3f7c5a6ae0128fefcd28ab4099872618
Monica Navarro <me.navarro.97@outlook.com>
2022-10-12 11:13:10 +0100

    User input for cost of trim and cost of window added and is successful

--- windowmaster/src/main/java/com/mycompany/windowmaster/Windowmaster.java
+++ windowmaster/src/main/java/com/mycompany/windowmaster/Windowmaster.java
@@ -17,10 +17,14 @@ public class Windowmaster {
         float height;
         float width; 
         float numberOfWindows;
+        float costOfWindow;
+        float costOfWindowTrim;
         
         String stringHeight;
         String stringWidth;
         String stringNumberOfWindows;
+        String stringCostOfWindow;
+        String stringCostOfWindowTrim;
         
         float areaOfWindow;
         float cost;
@@ -34,17 +38,25 @@ public class Windowmaster {
         stringWidth = myScanner.nextLine();
         System.out.println("Enter number of windows");
         stringNumberOfWindows = myScanner.nextLine();
+        System.out.println("Enter cost of window(s)");
+        stringCostOfWindow = myScanner.nextLine();
+        System.out.println("Enter cost of trim");
+        stringCostOfWindowTrim = myScanner.nextLine();
         
         height = Float.parseFloat(stringHeight);
         width = Float.parseFloat(stringWidth);
         numberOfWindows = Float.parseFloat(stringNumberOfWindows);
+        costOfWindow = Float.parseFloat(stringCostOfWindow);
+        costOfWindowTrim = Float.parseFloat(stringCostOfWindowTrim);
         
         areaOfWindow = numberOfWindows * (height * width);
         perimeterOfWindow = numberOfWindows * 2 * (height + width);
         
-        cost = ((3.50f * areaOfWindow) + (2.25f * perimeterOfWindow));
+        cost = ((costOfWindow * areaOfWindow) + (costOfWindowTrim * perimeterOfWindow));
         
         System.out.println("Number of windows = " + stringNumberOfWindows);
+        System.out.println("Cost of window = " + costOfWindow);
+        System.out.println("Cost of window trim = " + costOfWindowTrim);
         System.out.println("Window height = " + stringHeight);
         System.out.println("Window width = " + stringWidth);
         System.out.println("Window area = " + areaOfWindow);
