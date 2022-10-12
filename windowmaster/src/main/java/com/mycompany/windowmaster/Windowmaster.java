db5182fd8a54a81e08b41e137edb6445a340230f
Monica Navarro <me.navarro.97@outlook.com>
2022-10-12 10:13:51 +0100

    Declared variables

---
+++ windowmaster/src/main/java/com/mycompany/windowmaster/Windowmaster.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.windowmaster;
+
+/**
+ *
+ * @author JulianAcosta
+ */
+public class Windowmaster {
+
+    public static void main(String[] args) {
+        float height;
+        float width; 
+        
+        String stringHeight;
+        String stringWidth;
+        
+        float areaOfWindow;
+        float cost;
+        float perimeterOfWindow;
+    }
+}
