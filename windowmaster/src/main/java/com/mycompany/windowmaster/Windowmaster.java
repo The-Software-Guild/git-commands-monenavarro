e12f1ec82d999e059a944b438d4b1ebca338013d
Monica Navarro <me.navarro.97@outlook.com>
2022-10-12 10:49:55 +0100

    Added user input prompt for number of windows and returns successful output

--- windowmaster/src/main/java/com/mycompany/windowmaster/Windowmaster.java
+++ windowmaster/src/main/java/com/mycompany/windowmaster/Windowmaster.java
@@ -16,9 +16,11 @@ public class Windowmaster {
     public static void main(String[] args) {
         float height;
         float width; 
+        float numberOfWindows;
         
         String stringHeight;
         String stringWidth;
+        String stringNumberOfWindows;
         
         float areaOfWindow;
         float cost;
@@ -30,15 +32,19 @@ public class Windowmaster {
         stringHeight = myScanner.nextLine();
         System.out.println("Enter window width:");
         stringWidth = myScanner.nextLine();
+        System.out.println("Enter number of windows");
+        stringNumberOfWindows = myScanner.nextLine();
         
         height = Float.parseFloat(stringHeight);
         width = Float.parseFloat(stringWidth);
+        numberOfWindows = Float.parseFloat(stringNumberOfWindows);
         
-        areaOfWindow = height * width;
-        perimeterOfWindow = 2 * (height + width);
+        areaOfWindow = numberOfWindows * (height * width);
+        perimeterOfWindow = numberOfWindows * 2 * (height + width);
         
         cost = ((3.50f * areaOfWindow) + (2.25f * perimeterOfWindow));
         
+        System.out.println("Number of windows = " + stringNumberOfWindows);
         System.out.println("Window height = " + stringHeight);
         System.out.println("Window width = " + stringWidth);
         System.out.println("Window area = " + areaOfWindow);
