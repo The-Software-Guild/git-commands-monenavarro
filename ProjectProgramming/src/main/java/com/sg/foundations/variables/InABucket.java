3a99bcff5c6e476b41875b6ce628ddc21ee0472c
Monica Navarro <me.navarro.97@outlook.com>
2022-10-14 18:15:18 +0100

    In a Bucket task success

---
+++ ProjectProgramming/src/main/java/com/sg/foundations/variables/InABucket.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author JulianAcosta
+ */
+public class InABucket {
+    public static void main(String[] args) {
+        //declare variables
+        String walrus;
+        double piesEaten;
+        float weightOfTeacupPig;
+        int grainsOfSand;
+        
+        //assign data to each variable
+        walrus = "Nugget Lord";
+        piesEaten = 324.52;
+        weightOfTeacupPig = 1;
+        grainsOfSand = 88;
+        
+        System.out.println("Meet my pet walrus, " + walrus);
+        System.out.print("He was a bit hungry today, and ate this many pies: ");
+        System.out.println(piesEaten);
+        System.out.println("I also have a teacup piggy, who weighs: " + weightOfTeacupPig);
+        System.out.println("My teacup piggy eats roughly: " + grainsOfSand + " per day.");
+    }
+}
