23c51061f7894f2677cad2311f72d5271a6f084f
Monica Navarro <me.navarro.97@outlook.com>
2022-10-16 15:24:09 +0100

    Best Adder Ever success

---
+++ ProjectProgramming/src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sg.foundations.variables;
+
+/**
+ *
+ * @author JulianAcosta
+ */
+public class BestAdderEver {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        int num3 = 4;
+        
+        int totalSum = num1 + num2 + num3;
+        
+        System.out.println("I've created the Best Adder Program Ever!");
+        System.out.println(num1 + " + " + num2 + " + " + num3 + " = " + totalSum + ".");
+        
+    }
+}
