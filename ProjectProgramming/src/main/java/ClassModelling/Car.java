cab60dccb3747b684374e9945f35a7f2a018cb0a
Monica Navarro <me.navarro.97@outlook.com>
2022-10-18 13:51:33 +0100

    Car modelling

---
+++ ProjectProgramming/src/main/java/ClassModelling/Car.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClassModelling;
+
+/**
+ *
+ * @author JulianAcosta
+ */
+public class Car {
+    
+    private String carBrand;
+    private String carColour;
+    private String carTransmission;
+    private String carFuelType;
+    private char carTaxBand;
+    private int carYear;
+    private int carMileage;
+    private int carPrice;
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getCarColour() {
+        return carColour;
+    }
+
+    public void setCarColour(String carColour) {
+        this.carColour = carColour;
+    }
+
+    public String getCarTransmission() {
+        return carTransmission;
+    }
+
+    public void setCarTransmission(String carTransmission) {
+        this.carTransmission = carTransmission;
+    }
+
+    public String getCarFuelType() {
+        return carFuelType;
+    }
+
+    public void setCarFuelType(String carFuelType) {
+        this.carFuelType = carFuelType;
+    }
+
+    public char getCarTaxBand() {
+        return carTaxBand;
+    }
+
+    public void setCarTaxBand(char carTaxBand) {
+        this.carTaxBand = carTaxBand;
+    }
+
+    public int getCarYear() {
+        return carYear;
+    }
+
+    public void setCarYear(int carYear) {
+        this.carYear = carYear;
+    }
+
+    public int getCarMileage() {
+        return carMileage;
+    }
+
+    public void setCarMileage(int carMileage) {
+        this.carMileage = carMileage;
+    }
+
+    public int getCarPrice() {
+        return carPrice;
+    }
+
+    public void setCarPrice(int carPrice) {
+        this.carPrice = carPrice;
+    }
+    
+}
