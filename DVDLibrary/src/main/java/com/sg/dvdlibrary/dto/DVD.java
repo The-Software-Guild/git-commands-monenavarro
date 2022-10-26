96d2bd471322e11c15abfbbdc678eea3e83cca8b
Monica Navarro <me.navarro.97@outlook.com>
2022-10-26 15:49:39 +0100

    Created DVD class and implemented getters and setters

--- DVDLibrary/src/main/java/com/sg/dvdlibrary/dto/DVD.java
+++ DVDLibrary/src/main/java/com/sg/dvdlibrary/dto/DVD.java
@@ -8,6 +8,76 @@ package com.sg.dvdlibrary.dto;
  *
  * @author MonicaNavarro
  */
+
 public class DVD {
+    private String title;
+    private String releaseDate;
+    private String mpaaRating;
+    private String director;
+    private String studio;
+    private String userComment;
+    
+    public DVD() {
+        
+    }
+    
+    public DVD(String title, String releaseDate, String mpaaRating, String director, String studio, String userComment){
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.mpaaRating = mpaaRating;
+        this.director = director;
+        this.studio = studio;
+        this.userComment = userComment;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getmpaaRating() {
+        return mpaaRating;
+    }
+
+    public void setmpaaRating(String mpaaRating) {
+        this.mpaaRating = mpaaRating;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getuserComment() {
+        return userComment;
+    }
+
+    public void setuserComment(String userComment) {
+        this.userComment = userComment;
+    }
+    
+    
     
 }
--- DVDLibrary/src/main/java/com/sg/dvdlibrary/ui/DVDLibraryView.java
+++ DVDLibrary/src/main/java/com/sg/dvdlibrary/ui/DVDLibraryView.java
@@ -10,4 +10,16 @@ package com.sg.dvdlibrary.ui;
  */
 public class DVDLibraryView {
     
+    private UserIO io = new UserIOConsoleImpl();
+    
+    public int printMenuAndGetSelection() {
+        io.print("Main Menu");
+        io.print("1. List DVD Titles");
+        io.print("2. Add New DVD");
+        io.print("3. View a DVD");
+        io.print("4. Delete a DVD Entry");
+        io.print("5. Quit");
+        
+        return io.readInt("Please select from the options above.", 1, 5);
+    }
 }
